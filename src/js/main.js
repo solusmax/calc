2d09f5752354805dc81e95e09f6de1b4493af03b
solusmax <solusmaxim@gmail.com>
2021-05-12 02:57:58 +0300

    Убрал скрипт включения JS
    Калькулятор всё равно не заработает без JS,
    нет смысла думать о прогрессивном улучшении.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,9 @@
 import 'focus-visible';
-import { enableJs } from './util';
 import { generateButtons, BUTTONS_LAYOUT } from './buttons.js';
 import { renderButtons, setButtonsListeners, setInputListeners } from './render.js';
 import { fitInputContent } from './input.js';
 
 const Buttons = generateButtons();
-
-enableJs();
 
 renderButtons(Buttons, BUTTONS_LAYOUT);
 setButtonsListeners(Buttons);
--- src/js/util/index.js
+++ src/js/util/index.js
@@ -1,3 +1,2 @@
 export * from './common.js';
-export * from './enable-js.js';
 export * from './render.js';
